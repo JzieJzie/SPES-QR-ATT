858a2b69d222dc2e968725af6db7ca30c41abc04
JzieJzie <arambulo1jzieneloamran@gmail.com>
2026-03-21 10:34:05 +0800

    dev add

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useEffect } from 'react'
 import './index.css'
 import { AppProviders } from './app/providers'
 import { App } from './app/App'
 
+const GlobalHotkeys = () => {
+  useEffect(() => {
+    const onKeyDown = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() === 'a') {
+        event.preventDefault()
+        window.location.href = '/dev/developer.html'
+      }
+    }
+
+    window.addEventListener('keydown', onKeyDown)
+    return () => window.removeEventListener('keydown', onKeyDown)
+  }, [])
+
+  return null
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProviders>
+      <GlobalHotkeys />
       <App />
     </AppProviders>
   </StrictMode>,
